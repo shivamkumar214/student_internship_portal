b9ad38abcb7acd411cf53ac4ace0b673e98d3ecb
shivam <shivamkumar749362@gmail.com>
2026-07-08 15:34:39 +0530

    Added backend and updated frontend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
-  console.log("signup page")
+  console.log("signup page");
   try {
     const { name, email, phone, college, course, password } = req.body;
 
@@ -16,7 +16,30 @@ export const signup = async (req, res) => {
       });
     }
 
+    // const hashedPassword = await bcrypt.hash(password, 10);
+
+    // const user = await User.create({
+    //   name,
+    //   email,
+    //   phone,
+    //   college,
+    //   course,
+    //   password: hashedPassword,
+    // });
+
+    // const newUser = await User.findById(user._id).select("-password");
+    // const token = jwt.sign({ id: newUser._id, }, process.env.JWT_SECRET,
+    //   {
+    //     expiresIn: "7d",
+    //   },
+    // );
+    // console.log(token);
+
+    console.log("1");
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    console.log("2");
 
     const user = await User.create({
       name,
@@ -27,13 +50,24 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
 
+    console.log("3");
+
     const newUser = await User.findById(user._id).select("-password");
-    res.cookie("token",token,{
-        httpOnly:true,
-        maxAge:7*24*60*60*1000, //7day = 7*24hr*60mint*60sec*1000milliseconds
-        sameSite:"strict",
-        secure:false
-    })
+
+    console.log("4");
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    console.log("5", token);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, //7day = 7*24hr*60mint*60sec*1000milliseconds
+      sameSite: "strict",
+      secure: false,
+    });
 
     res.status(201).json({
       success: true,
@@ -70,19 +104,17 @@ export const login = async (req, res) => {
       });
     }
 
-    const token = jwt.sign({ id: user._id, }, process.env.JWT_SECRET,
-      {
-        expiresIn: "7d",
-      },
-    );
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
     const userData = await User.findById(user._id).select("-password");
 
-    res.cookie("token",token,{
-        httpOnly:true,
-        maxAge:7*24*60*60*1000, //7day = 7*24hr*60mint*60sec*1000milliseconds
-        sameSite:"strict",
-        secure:false
-    })
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, //7day = 7*24hr*60mint*60sec*1000milliseconds
+      sameSite: "strict",
+      secure: false,
+    });
 
     res.status(200).json({
       success: true,
