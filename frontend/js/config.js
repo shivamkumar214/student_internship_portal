3edf054c7ca43ece3fcf6ae3df101862d01bcc39
shivam <shivamkumar749362@gmail.com>
2026-07-10 14:47:01 +0530

    /

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
 // const BASE_URL = "http://127.0.0.1:5000";
-export const BASE_URL = "https://student-internship-portal-lvvf.onrender.com/";
+export const BASE_URL = "https://student-internship-portal-lvvf.onrender.com";
 // export default BASE_URL;
